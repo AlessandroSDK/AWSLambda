de7b15c78877354adb23824c152a51ea998b06af
Alessandro Bresciani <Alessandro@iMac.lan>
2017-04-17 15:36:51 +0100

    adding event.json and simple lambda

--- handler.js
+++ handler.js
@@ -1,12 +1,18 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
+  console.log('event is', event);
+  console.log('context is', context);
+  let remainingTime = context.getRemainingTimeInMillis();
+  let functionName = context.functionName;
+  let AWSrequestID = context.awsRequestId;
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+        ev: event,
+        rt: remainingTime,
+    })
   };
 
   callback(null, response);
